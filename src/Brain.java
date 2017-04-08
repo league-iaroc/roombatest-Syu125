fa9d4794d9d736dec6a42912963e2b2331379fe7
Syu125 <league@WTS-IM-17.attlocal.net>
2017-04-08 16:01:08 -0700

    Roomba!

--- src/Brain.java
+++ src/Brain.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 
 /***
- * This is the  class where the student will write their robot code.
+ * This is the class where the student will write their robot code.
  * 
  * @author keith
  *
@@ -23,28 +23,14 @@ public class Brain extends Head {
 	 * (think of "setup method in processing")
 	 */
 	public void initialize() {
-
-		// drive direct will move the roomba and takes a wheelspeed
 		driveDirect(-500, -500);
-
-		// This will continue the last command for 2000 milliseconds.
-		sleep(2000);
-
-		// differing wheel speeds will cause the roomba to turn
-		driveDirect(500, 200);
-		sleep(1000);
 	}
 
 	/**
 	 * The loop method repeats (think of the draw method)
 	 */
 	public void loop() {
-		driveDirect(600, 500);
-		if (isBumpedRight() || isBumpedLeft()) {
-			// This code will execute if the roomba hits a wall
-			driveDirect(-500, -500);
-			sleep(1000);
-		}
+
 	}
 
 }
